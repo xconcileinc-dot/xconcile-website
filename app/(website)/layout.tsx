75f7d3f433a9db665763f4af6499c1d84ccd6b32
pratik-senjaliya <pratiksenjaliya7@gmail.com>
2026-01-27 23:10:24 +0530

    GTM added

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -96,8 +96,29 @@ export default function RootLayout({
             __html: JSON.stringify(organizationSchema),
           }}
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WRRFT9ZZ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className="min-h-screen antialiased flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRRFT9ZZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
